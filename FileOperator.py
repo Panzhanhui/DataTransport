1d2f5db203e0d81c487cc964ba7b757182013921
bill <2966231784@qq.com>
2024-02-05 22:02:39 +0800

    v0.0.0.1

--- FileOperator.py
+++ FileOperator.py
@@ -1,10 +1,12 @@
+file_data=None
 def drop(event):
+    global file_data
     file_path = event.data
     if file_path:
         try:
             with open(file_path, 'r') as file:
                 file_data = file.read()
-                return file_data
+                
                 # label_result.config(text=f'文件数据:\n{file_data}')
         except Exception as e:
-            return '读取文件时发生错误'
+            print ('读取文件时发生错误')
