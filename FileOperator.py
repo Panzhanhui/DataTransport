b64c44e9331af96a762c989ce4885505aafae371
bill <2966231784@qq.com>
2024-02-05 22:43:46 +0800

    v0.0.0.2

--- FileOperator.py
+++ FileOperator.py
@@ -4,9 +4,13 @@ def drop(event):
     file_path = event.data
     if file_path:
         try:
-            with open(file_path, 'r') as file:
+            with open(file_path, 'r',encoding='utf-8') as file:
                 file_data = file.read()
-                
+                print('===',file_data)
+                # return file_data
                 # label_result.config(text=f'文件数据:\n{file_data}')
         except Exception as e:
             print ('读取文件时发生错误')
+def on_drag_leave(event):
+ 
+        event.widget.configure(bg='white')    
--- Main.py
+++ Main.py
@@ -1,31 +1,55 @@
+import threading
 import tkinter as tk
 from tkinter import filedialog
 from tkinterdnd2 import TkinterDnD, DND_FILES
 import FileOperator as fo
 
+file_data = None
+
+
 class Mainwindow:
-    
+
     def open_file_dialog(self):
-        file_path = filedialog.askopenfilename(title="选择文件", filetypes=[("文本文件", "*.txt"), ("所有文件", "*.*")])
+        file_path = filedialog.askopenfilename(
+            title="选择文件", filetypes=[("文本文件", "*.txt"), ("所有文件", "*.*")])
         if file_path:
             # label_result.config(text=f'选择的文件路径：{file_path}')
             pass
 
+    def updateLabel(self):
+        global file_data
+        while True:
+            print(file_data)
+            if not file_data:
+                file_data = fo.file_data
+
     def onCreate(self):
+        global file_data
         # create a window
         window = TkinterDnD.Tk()
-    
+
+        drop = fo.drop
 
         window.drop_target_register(DND_FILES)
-        window.dnd_bind('<<Drop>>', fo.drop)
+        window.dnd_bind('<<Drop>>', drop)
+        window.dnd_bind('<Leave>', fo.on_drag_leave)
+
         # 创建编辑框
         entry = tk.Entry(window, width=30)
         entry.pack(pady=10)
 
         # create a label
-        label_result = tk.Label(window)
+        label_result = tk.Label(window, width=40, height=10,
+                                relief="solid", borderwidth=1, anchor='nw', justify='left')
         label_result.pack(pady=10)
 
-        Mainwindow().open_file_dialog()
+        # Mainwindow().open_file_dialog()
+        print(file_data, '===')
+        t = threading.Thread(target=Mainwindow().updateLabel)
+        t.start()
+        label_result.config(text='file_data')
+
         window.mainloop()
+
+
 Mainwindow().onCreate()
